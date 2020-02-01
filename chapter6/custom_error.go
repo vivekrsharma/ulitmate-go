88faea1a9271b113f852119b83b1f49849daadcc
vrs <vivekrajeshsharma@gmail.com>
2020-01-31 17:55:44 -0800

    [chapter 6]: Add example of pointer semantics for error

---
+++ chapter6/custom_error.go
@@ -0,0 +1,43 @@
+package chapter6
+
+import "fmt"
+
+type customError struct{
+	message string
+}
+
+func (c customError) Error() string {
+	return c.message
+}
+
+func New(str string) error {
+	return customError{message:str}
+}
+
+// Following is the right way to create custom errors.
+//func (c *customError) Error() string {
+//	return c.message
+//}
+//
+//func New(str string) error {
+//	return &customError{message:str}
+//}
+func webCall() error {
+	return New("Bad Request")
+}
+
+func main() {
+	fakeError := New("Bad Request")
+	if err := webCall(); err!=nil {
+		fmt.Println("This is a webCall error.")
+		if fakeError == err {
+			fmt.Println("Insanity.. ")
+			panic(err)
+		}
+		fmt.Println("Life is good.")
+	}
+	fmt.Println("Life is amazing.")
+
+}
+
+
