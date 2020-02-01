a43b9c35ce79254943168304dcddbeac50d6834c
vrs <vivekrajeshsharma@gmail.com>
2020-01-31 17:31:05 -0800

    [chapter 8]: Add examples/usage for waitGroup and synchronization

---
+++ chapter8/simple_multithreaded_print.go
@@ -0,0 +1,46 @@
+package chapter8
+
+import (
+	"fmt"
+	"runtime"
+	"sync"
+)
+
+func init() {
+	runtime.GOMAXPROCS(2)
+}
+
+func lowercase() {
+	for ch :='a'; ch <='z'; ch++ {
+		fmt.Printf("%c",ch)
+	}
+	fmt.Println("Done")
+}
+
+func uppercase() {
+	for ch :='A'; ch <='Z'; ch++ {
+		fmt.Printf("%c",ch)
+	}
+	fmt.Println("Done")
+}
+
+func main() {
+	var wg sync.WaitGroup
+	wg.Add(2)
+
+	go func() {
+		for i:=0;i<5;i++ {
+			lowercase()
+		}
+		wg.Done()
+	}()
+
+	go func() {
+		for i:=0;i<5;i++ {
+			uppercase()
+		}
+		wg.Done()
+	}()
+
+	wg.Wait()
+}
---
+++ chapter8/simple_mutlithread_prime.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+	"sync"
+)
+
+func primeNumber(n int, name string) {
+	for i:=1; i<=n;i++ {
+		isPrime := true
+		for j:=2;j<i;j++ {
+			if i %j == 0 {
+				isPrime = false
+				break;
+			}
+		}
+		if isPrime {
+			fmt.Printf("%s: %d \n", name, i)
+		}
+	}
+}
+
+func init() {
+	runtime.GOMAXPROCS(2)
+}
+
+func main() {
+	fmt.Println("Hello World!")
+	var wg sync.WaitGroup
+	wg.Add(2)
+
+	go func() {
+		primeNumber(5000, "A")
+		wg.Done()
+	}()
+
+	go func (){
+		primeNumber(5000, "B")
+		wg.Done()
+	}()
+
+	wg.Wait()
+}
