8e22bdc47c3695c9c06ba984211101ec4872b9e4
vrs <vivekrajeshsharma@gmail.com>
2020-02-01 20:51:35 -0800

    [chapter 11]: Add examples to handle concurrency with channels, context

---
+++ chapter11/example1.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func contextTimeout() {
+
+	ctx, cancel := context.WithTimeout(context.Background(), 50 * time.Millisecond)
+	defer cancel()
+
+	ch := make(chan string, 1)
+
+	go func() {
+		time.Sleep(time.Duration(rand.Intn(500)) * time.Millisecond)
+		ch <- "123"
+	}()
+
+	select {
+		case <-ch :
+			fmt.Println("Task completed successfully.")
+		case <-ctx.Done() :
+			fmt.Println("The action is timedout.")
+	}
+}
+
+
+func main()   {
+	contextTimeout()
+}
